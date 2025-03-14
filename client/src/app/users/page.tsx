c3e38eadf5b1ca45b4e78202d67b50789bcc32c0
unknown <ntt31035@gmail.com>
2025-03-14 09:09:30 +0700

    add client

---
+++ client/src/app/users/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useGetUsersQuery } from "@/state/api";
+import React from "react";
+import Header from "@/app/(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "userId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "email", headerName: "Email", width: 200 },
+];
+
+const Users = () => {
+  const { data: user, isError, isLoading } = useGetUsersQuery();
+  if (isLoading) {
+    return <div className="py-4">Loading...</div>;
+  }
+  if (isError || !user) {
+    return (
+      <div className="text-center text-red-500 py-4">Failed to fetch user</div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <Header name="Users" />
+      <DataGrid
+        rows={user}
+        columns={columns}
+        getRowId={(row) => row.userId}
+        checkboxSelection
+        className="bg-white shadow rounded-lg border border-gray-200 mt-5 text-gray-700"
+      />
+    </div>
+  );
+};
+
+export default Users;
